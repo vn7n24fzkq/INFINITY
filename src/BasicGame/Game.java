a14469951c8559ab5122341b757d7670abaf2332
vn7 <vn7n24fzkq.gmail.com>
2017-11-02 16:15:18 +0800

    Remove redundant annotations

--- src/BasicGame/Game.java
+++ src/BasicGame/Game.java
@@ -9,7 +9,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Locale;
 
-
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.Color;
@@ -28,10 +27,9 @@ import state.Play;
 import state.StartMenu;
 import state.state;
 
-
 public class Game extends StateBasedGame {
-//	public static  Listener keyListener = new Listener();
-	
+	// public static Listener keyListener = new Listener();
+
 	public Game(String name) {
 		super(name);
 		this.addState(new StartMenu(state.StartMenu));
@@ -44,59 +42,37 @@ public class Game extends StateBasedGame {
 	@Override
 	public void initStatesList(GameContainer gc) throws SlickException {
 		new Resources();
-	//	this.getState(state.StartMenu).init(gc, this);
-	//	this.getState(state.Play).init(gc, this);
-	//	this.getState(state.Menu).init(gc, this);
-	//	this.enterState(state.StartMenu);
 		// gc.setMouseGrabbed(true);
 		Control.input = gc.getInput();
-	
-		
+
 	}
 
 	public static void main(String[] args) throws SlickException {
 		AppGameContainer app = new AppGameContainer(new Game(window.gamename));
-			
+
 		app.setDisplayMode(window.WIDTH, window.HEIGHT, window.full);
 		// .setDisplayMode(¼e,°ª, ¬O§_¥þ¿Ã¹õ);
-	//	app.setAlwaysRender(true);
-	//	app.setMinimumLogicUpdateInterval(10);
+		// app.setAlwaysRender(true);
+		// app.setMinimumLogicUpdateInterval(10);
 		app.setAlwaysRender(false);
 		app.setMinimumLogicUpdateInterval(15);
 		app.setMaximumLogicUpdateInterval(15);
 		app.setTargetFrameRate(window.FPS);
 		app.setShowFPS(false);
 		app.setAlwaysRender(false);
-		
-	//	app.setVSync(true);
-		//app.setSmoothDeltas(true);
-		//app.setDefaultMouseCursor();
-	//	app.setMouseGrabbed(true);
-	//	Mouse.setGrabbed(true);
-	
+
+		// app.setVSync(true);
+		// app.setSmoothDeltas(true);
+		// app.setDefaultMouseCursor();
+		// app.setMouseGrabbed(true);
+		// Mouse.setGrabbed(true);
+
 		try {
-
 			app.start();
-			
-			//app.getInput().addKeyListener(keyListener);
-		} catch (Exception e) {
-/*
-			FileWriter fw;
-			try {
-				fw = new FileWriter(DataPath.path+"/toast.txt");
-				BufferedWriter bw = new BufferedWriter(fw, 20);
-				bw.write(e.toString());
-				bw.newLine();
-				bw.flush();
-				bw.close();
 
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-*/
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
-	
 
 }
--- src/BasicGame/Resources.java
+++ src/BasicGame/Resources.java
@@ -31,55 +31,10 @@ public class Resources {
 		images = new HashMap<String, Image>();
 		sprites = new HashMap<String, SpriteSheet>();
 		FileWriter fw;
-	//	System.out.println(System.getProperty("user.dir"));
-	//	System.out.println( (System.getProperty("user.dir")).replace("\\","/"));
-		/*try {
-			fw = new FileWriter((System.getProperty("user.dir"))+"\\toast.txt");
-			BufferedWriter bw = new BufferedWriter(fw, 20);
-			bw.write((System.getProperty("user.dir")).replace("\\","/"));
-			bw.newLine();
-			bw.flush();
-			bw.close();
-
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}*/
-		
 		try {
 			// read map tmx
 			//map.put("test", loadMap( Data.DataPath.path + "/tiled/test.tmx"));
-
 			// load charactersheet
-		/*	sprites.put("chaStandRight", loadSprite(path + "/character/davis/davis_stand_right.png",
-					PlayerData.PlayerSheetWIDTH, PlayerData.PlayerSheetHEIGHT));
-			sprites.put("chaStandLeft", loadSprite(path + "/character/davis/davis__stand_left.png",
-					PlayerData.PlayerSheetWIDTH, PlayerData.PlayerSheetHEIGHT));
-			sprites.put("chaWalkRight", loadSprite(path + "/character/davis/davis_walk_right.png",
-					PlayerData.PlayerSheetWIDTH, PlayerData.PlayerSheetHEIGHT));
-			sprites.put("chaWalkLeft", loadSprite(path + "/character/davis/davis_walk_left.png",
-					PlayerData.PlayerSheetWIDTH, PlayerData.PlayerSheetHEIGHT));
-			sprites.put("chaJumpRight", loadSprite(path + "/character/davis/davis_jump_right.png",
-					PlayerData.PlayerSheetWIDTH, PlayerData.PlayerSheetHEIGHT));
-			sprites.put("chaJumpLeft", loadSprite(path + "/character/davis/davis_jump_left.png",
-					PlayerData.PlayerSheetWIDTH, PlayerData.PlayerSheetHEIGHT));
-			sprites.put("chaSquatRight", loadSprite(path + "/character/davis/davis_squat_right.png",
-					PlayerData.PlayerSheetWIDTH, PlayerData.PlayerSheetHEIGHT));
-			sprites.put("chaSquatLeft", loadSprite(path + "/character/davis/davis_squat_left.png",
-					PlayerData.PlayerSheetWIDTH, PlayerData.PlayerSheetHEIGHT));
-			sprites.put("cha_Z_AttackRight", loadSprite(path + "/character/davis/davis_z_attack_right.png",
-					PlayerData.PlayerSheetWIDTH, PlayerData.PlayerSheetHEIGHT));
-			sprites.put("cha_Z_AttackLeft", loadSprite(path + "/character/davis/davis_z_attack_left.png",
-					PlayerData.PlayerSheetWIDTH, PlayerData.PlayerSheetHEIGHT));
-			sprites.put("cha_X_AttackRight", loadSprite(path + "/character/davis/davis_x_attack_right.png",
-					PlayerData.PlayerSheetWIDTH, PlayerData.PlayerSheetHEIGHT));
-			sprites.put("cha_X_AttackLeft", loadSprite(path + "/character/davis/davis_x_attack_left.png",
-					PlayerData.PlayerSheetWIDTH, PlayerData.PlayerSheetHEIGHT));
-			sprites.put("cha_HurtRight", loadSprite(path + "/character/davis/davis_hurt_right.png",
-					PlayerData.PlayerSheetWIDTH, PlayerData.PlayerSheetHEIGHT));
-			sprites.put("cha_HurtLeft", loadSprite(path + "/character/davis/davis_hurt_left.png",
-					PlayerData.PlayerSheetWIDTH, PlayerData.PlayerSheetHEIGHT));
-			*/
 			sprites.put("life_full", loadSprite(Data.DataPath.path_res_pic_UI + "/life_full.png",
 					40, 32));
 			sprites.put("life_empty", loadSprite(Data.DataPath.path_res_pic_UI + "/life_empty.png",
@@ -91,7 +46,6 @@ public class Resources {
 
 			// reade opening background image
 			images.put("background", loadImage(Data.DataPath.path_res_back + "/background.png"));
-
 			// read map png
 			//images.put("back", loadImage(Data.DataPath.path + "/tiled/test_back.png"));
 
